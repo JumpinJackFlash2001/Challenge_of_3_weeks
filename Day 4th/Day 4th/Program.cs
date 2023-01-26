174e63f56272f87c8667c8e12ae4b9cfb91ca722
JumpinJackFlash2001 <ad.rozycki@gmail.com>
2023-01-27 00:26:38 +0100

    Commit dnia czwartego

---
+++ Day 4th/Day 4th/Program.cs
@@ -0,0 +1,21 @@
+﻿// Projekt na dzień 4
+string name = "Bonawentura";
+Console.WriteLine(name);
+var gender = "Mężczyzna";
+var age = 17;
+if (age >= 18 && gender == "Mężczyzna")
+    {
+    Console.WriteLine("Pełnoletni Mężczyzna");
+    }
+if (age < 18 && gender == "Mężczyzna")
+    {
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+if (age < 18 && gender != "Mężczyzna")
+    {
+    Console.WriteLine("Niepełnoletnia Kobieta");
+    }
+if (age >= 18 && gender != "Mężczyzna")
+    {
+    Console.WriteLine("Pełnoletnia Kobieta");
+    }
