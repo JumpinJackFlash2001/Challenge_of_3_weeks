66e76e617b23a6eabd21bd869ec92fb69ed431ef
JumpinJackFlash2001 <ad.rozycki@gmail.com>
2023-01-30 23:42:32 +0100

    Day 5th stworzenie aplikacji

---
+++ Day 5th/Day 5th/Program.cs
@@ -0,0 +1,71 @@
+﻿// Projekt na dzień 5
+
+int number = 11155589;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0; 
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach(char letter in letters)
+{
+    if(letter == '0')
+    {
+    counter0++;
+    }
+    else if(letter == '1')
+    {
+    counter1++;
+    }
+    else if(letter == '2')
+    {
+    counter2++;
+    }
+    else if(letter == '3')
+    {
+    counter3++;
+    }
+    else if(letter == '4')
+    {
+    counter4++;
+    }
+    else if(letter == '5')
+    {
+    counter5++;
+    }
+    else if(letter == '6')
+    {
+    counter6++;
+    }
+    else if(letter == '7')
+    {
+    counter7++;
+    }
+    else if(letter =='8')
+    {
+    counter8++;
+    }
+    else if(letter == '9')
+    {
+    counter9++;
+    }
+}
+Console.WriteLine(number);
+Console.WriteLine("ilość 0 = "+counter0);
+Console.WriteLine("ilość 1 = "+counter1);
+Console.WriteLine("ilość 2 = "+counter2);
+Console.WriteLine("ilość 3 = "+counter3);
+Console.WriteLine("ilość 4 = "+counter4);
+Console.WriteLine("ilość 5 = "+counter5);
+Console.WriteLine("ilość 6 = "+counter6);
+Console.WriteLine("ilość 7 = "+counter7);
+Console.WriteLine("ilość 8 = "+counter8);
+Console.WriteLine("ilość 9 = "+counter9);    
